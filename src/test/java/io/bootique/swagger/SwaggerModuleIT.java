569536d6b3c0eaf1270a7c20c2656447ce1532ca
Andrus Adamchik <andrus@objectstyle.com>
2016-06-25 10:05:39 -0400

    better tests

--- src/test/java/io/bootique/swagger/SwaggerModuleIT.java
+++ src/test/java/io/bootique/swagger/SwaggerModuleIT.java
@@ -4,7 +4,8 @@ import com.nhl.bootique.jersey.JerseyModule;
 import com.nhl.bootique.jetty.JettyModule;
 import com.nhl.bootique.jetty.test.junit.JettyTestFactory;
 import io.swagger.annotations.Api;
-import org.junit.Rule;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 
 import javax.ws.rs.GET;
@@ -12,7 +13,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
-
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -20,21 +20,33 @@ import static org.junit.Assert.assertEquals;
 
 public class SwaggerModuleIT {
 
+    @ClassRule
+    public static JettyTestFactory testFactory = new JettyTestFactory();
+
     private static WebTarget BASE_TARGET = ClientBuilder.newClient().target("http://127.0.0.1:8080/");
-    @Rule
-    public JettyTestFactory testFactory = new JettyTestFactory();
 
-    @Test
-    public void testApi() {
-
+    @BeforeClass
+    public static void beforeClass() {
         testFactory.newRuntime().configurator(bootique -> {
             bootique.modules(JettyModule.class, JerseyModule.class, SwaggerModule.class)
                     .module(binder -> JerseyModule.contributeResources(binder).addBinding().to(TestApi.class));
         }).startServer();
+    }
+
+    @Test
+    public void testApi_Yaml() {
 
         Response r = BASE_TARGET.path("/swagger.yaml").request().get();
         assertEquals(200, r.getStatus());
         assertEqualsToResourceContents("response1.yml", r.readEntity(String.class));
+    }
+
+    @Test
+    public void testApi_Json() {
+
+        Response r = BASE_TARGET.path("/swagger.json").request().get();
+        assertEquals(200, r.getStatus());
+        assertEqualsToResourceContents("response1.json", r.readEntity(String.class) + "\n");
     }
 
     private void assertEqualsToResourceContents(String expectedResource, String toTest) {
@@ -43,7 +55,7 @@ public class SwaggerModuleIT {
                 Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(expectedResource)), "UTF-8")) {
 
             StringBuilder builder = new StringBuilder();
-            while(scanner.hasNextLine()) {
+            while (scanner.hasNextLine()) {
                 builder.append(scanner.nextLine()).append("\n");
             }
 
