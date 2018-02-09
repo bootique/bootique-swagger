6e33fa28c118ee4590264a517299466c004b4086
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-02-09 16:58:41 +0300

    Declare module dependencies bootique/bootique#203

--- src/main/java/io/bootique/swagger/SwaggerModuleProvider.java
+++ src/main/java/io/bootique/swagger/SwaggerModuleProvider.java
@@ -2,11 +2,22 @@ package io.bootique.swagger;
 
 import com.google.inject.Module;
 import io.bootique.BQModuleProvider;
+import io.bootique.jersey.JerseyModuleProvider;
+
+import java.util.Collection;
+import java.util.Collections;
 
 public class SwaggerModuleProvider implements BQModuleProvider {
 
     @Override
     public Module module() {
         return new SwaggerModule();
+    }
+
+    @Override
+    public Collection<BQModuleProvider> dependencies() {
+        return Collections.singletonList(
+                new JerseyModuleProvider()
+        );
     }
 }
--- src/test/java/io/bootique/swagger/SwaggerModuleProviderTest.java
+++ src/test/java/io/bootique/swagger/SwaggerModuleProviderTest.java
@@ -1,12 +1,30 @@
 package io.bootique.swagger;
 
+import io.bootique.BQRuntime;
+import io.bootique.jersey.JerseyModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQTestFactory;
+import org.junit.Rule;
 import org.junit.Test;
 
+import static com.google.common.collect.ImmutableList.of;
+
 public class SwaggerModuleProviderTest {
+
+    @Rule
+    public BQTestFactory testFactory = new BQTestFactory();
 
     @Test
     public void testPresentInJar() {
         BQModuleProviderChecker.testPresentInJar(SwaggerModuleProvider.class);
+    }
+
+    @Test
+    public void testModuleDeclaresDependencies() {
+        final BQRuntime bqRuntime = testFactory.app().module(new SwaggerModuleProvider()).createRuntime();
+        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+                JerseyModule.class,
+                SwaggerModule.class
+        ));
     }
 }
