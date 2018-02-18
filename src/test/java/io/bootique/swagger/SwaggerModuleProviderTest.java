1d0d757fbc4cc6ff865c66c395cb9b184ae67d76
Andrus Adamchik <andrus@objectstyle.com>
2018-02-18 10:39:10 +0300

    Declare module dependencies bootique/bootique#203
    * new test API

--- src/test/java/io/bootique/swagger/SwaggerModuleProviderTest.java
+++ src/test/java/io/bootique/swagger/SwaggerModuleProviderTest.java
@@ -3,11 +3,10 @@ package io.bootique.swagger;
 import io.bootique.BQRuntime;
 import io.bootique.jersey.JerseyModule;
 import io.bootique.test.junit.BQModuleProviderChecker;
+import io.bootique.test.junit.BQRuntimeChecker;
 import io.bootique.test.junit.BQTestFactory;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static com.google.common.collect.ImmutableList.of;
 
 public class SwaggerModuleProviderTest {
 
@@ -22,9 +21,9 @@ public class SwaggerModuleProviderTest {
     @Test
     public void testModuleDeclaresDependencies() {
         final BQRuntime bqRuntime = testFactory.app().module(new SwaggerModuleProvider()).createRuntime();
-        BQModuleProviderChecker.testModulesLoaded(bqRuntime, of(
+        BQRuntimeChecker.testModulesLoaded(bqRuntime,
                 JerseyModule.class,
                 SwaggerModule.class
-        ));
+        );
     }
 }
