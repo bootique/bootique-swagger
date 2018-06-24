e1f15d279fcf907cda3378d272580c5a065879e4
Andrus Adamchik <andrus@objectstyle.com>
2018-06-24 11:28:13 +0300

    Add integration with Swagger web console #1
    * removing direct references to javax.validation.. This jar comes as a dependency from
      Jersey. Bootique code should not rely on it

--- bootique-swagger-console/src/main/java/io/bootique/swagger/console/SwaggerConsoleModule.java
+++ bootique-swagger-console/src/main/java/io/bootique/swagger/console/SwaggerConsoleModule.java
@@ -5,7 +5,6 @@ import io.bootique.BQCoreModule;
 import io.bootique.ConfigModule;
 import io.bootique.jetty.JettyModule;
 
-import javax.validation.constraints.NotNull;
 import java.net.URL;
 
 /**
@@ -16,7 +15,7 @@ public class SwaggerConsoleModule extends ConfigModule {
     public static final String RESOURCE_BASE = "bq.jetty.servlets.swagger-ui.params.resourceBase";
 
     @Override
-    public void configure(@NotNull final Binder binder) {
+    public void configure(Binder binder) {
         URL resource = this.getClass().getClassLoader().getResource("console/");
         BQCoreModule.extend(binder).setProperty(RESOURCE_BASE, resource.toString());
         JettyModule.extend(binder).addStaticServlet("swagger-ui", "/swagger-ui/*");
--- bootique-swagger/src/main/java/io/bootique/swagger/SwaggerModule.java
+++ bootique-swagger/src/main/java/io/bootique/swagger/SwaggerModule.java
@@ -24,12 +24,10 @@ import io.bootique.ConfigModule;
 import io.bootique.jersey.JerseyModule;
 import io.swagger.jaxrs.listing.ApiListingResource;
 
-import javax.validation.constraints.NotNull;
-
 public class SwaggerModule extends ConfigModule {
 
     @Override
-    public void configure(@NotNull final Binder binder) {
+    public void configure(Binder binder) {
         JerseyModule.extend(binder).addPackage(ApiListingResource.class);
     }
 }
