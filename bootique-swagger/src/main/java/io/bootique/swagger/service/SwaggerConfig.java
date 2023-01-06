18b4999a651551d0a4de386f396465bedcbdee78
Max Novik <mnovik@objectstyle.com>
2023-01-06 12:40:34 +0300

    Fix formatting.

--- bootique-swagger/src/main/java/io/bootique/swagger/service/SwaggerConfig.java
+++ bootique-swagger/src/main/java/io/bootique/swagger/service/SwaggerConfig.java
@@ -21,39 +21,40 @@ package io.bootique.swagger.service;
 import io.bootique.annotation.BQConfig;
 import io.bootique.annotation.BQConfigProperty;
 import io.bootique.resource.ResourceFactory;
+
 import java.util.Map;
 
 @BQConfig
 public class SwaggerConfig {
-  private ResourceFactory overrideSpec;
-  private Map<String, SwaggerSpec> specs;
-  private boolean pretty = true;
+    private ResourceFactory overrideSpec;
+    private Map<String, SwaggerSpec> specs;
+    private boolean pretty = true;
 
-  public ResourceFactory getOverrideSpec() {
-    return overrideSpec;
-  }
+    public ResourceFactory getOverrideSpec() {
+        return overrideSpec;
+    }
 
-  public Map<String, SwaggerSpec> getSpecs() {
-    return specs;
-  }
+    public Map<String, SwaggerSpec> getSpecs() {
+        return specs;
+    }
 
-  public boolean isPretty() {
-    return pretty;
-  }
+    public boolean isPretty() {
+        return pretty;
+    }
 
-  @BQConfigProperty("Zero or more API specifications provided by the application")
-  public void setSpecs(Map<String, SwaggerSpec> specs) {
-    this.specs = specs;
-  }
+    @BQConfigProperty("Zero or more API specifications provided by the application")
+    public void setSpecs(Map<String, SwaggerSpec> specs) {
+        this.specs = specs;
+    }
 
-  @BQConfigProperty("Location of the OpenAPI spec file, that overrides 'spec', 'resourcePackages', 'resourceClasses' models. " +
-      "This setting is shared by all child specs, unless they define an explicit 'overrideSpec' of their own")
-  public void setOverrideSpec(ResourceFactory overrideSpec) {
-    this.overrideSpec = overrideSpec;
-  }
+    @BQConfigProperty("Location of the OpenAPI spec file, that overrides 'spec', 'resourcePackages', 'resourceClasses' models. " +
+            "This setting is shared by all child specs, unless they define an explicit 'overrideSpec' of their own")
+    public void setOverrideSpec(ResourceFactory overrideSpec) {
+        this.overrideSpec = overrideSpec;
+    }
 
-  @BQConfigProperty("Whether to format YAML and JSON. 'True' by default. This setting is shared by all child specs.")
-  public void setPretty(boolean pretty) {
-    this.pretty = pretty;
-  }
+    @BQConfigProperty("Whether to format YAML and JSON. 'True' by default. This setting is shared by all child specs.")
+    public void setPretty(boolean pretty) {
+        this.pretty = pretty;
+    }
 }
--- bootique-swagger/src/main/java/io/bootique/swagger/web/SwaggerApi.java
+++ bootique-swagger/src/main/java/io/bootique/swagger/web/SwaggerApi.java
@@ -34,39 +34,41 @@ import java.util.Objects;
 @Path("_this_is_a_placeholder_that_will_be_replaced_dynamically_")
 public class SwaggerApi {
 
-  private static final String MEDIA_TYPE_JSON = MediaType.APPLICATION_JSON;
-  private static final String MEDIA_TYPE_YAML= "application/yaml";
+    private static final String MEDIA_TYPE_JSON = MediaType.APPLICATION_JSON;
+    private static final String MEDIA_TYPE_YAML = "application/yaml";
 
-  private final SwaggerService service;
+    private final SwaggerService service;
 
-  public SwaggerApi(SwaggerService service) {
-    this.service = service;
-  }
+    public SwaggerApi(SwaggerService service) {
+        this.service = service;
+    }
 
-  @GET
-  @Produces({MEDIA_TYPE_JSON, MEDIA_TYPE_YAML})
-  @Operation(hidden = true)
-  public Response getOpenApi(@Context UriInfo uriInfo) {
-    var path = uriInfo.getPath();
-    return service.getOpenApiModel(path)
-        .map(model -> toResponse(path, model))
-        .orElseGet(() -> notFound(path));
-  }
-  private Response toResponse(String path, OpenApiModel model) {
-    var mediaType = getMediaType(path, model);
-    return Response.status(Response.Status.OK)
-        .entity(model.render(path))
-        .type(mediaType)
-        .build();
-  }
-  private Response notFound(String path) {
-    return Response
-        .status(Response.Status.NOT_FOUND)
-        .type(MediaType.TEXT_PLAIN_TYPE)
-        .entity("No model at " + path).build();
-  }
+    @GET
+    @Produces({MEDIA_TYPE_JSON, MEDIA_TYPE_YAML})
+    @Operation(hidden = true)
+    public Response getOpenApi(@Context UriInfo uriInfo) {
+        var path = uriInfo.getPath();
+        return service.getOpenApiModel(path)
+                .map(model -> toResponse(path, model))
+                .orElseGet(() -> notFound(path));
+    }
 
-  private String getMediaType(String path, OpenApiModel model) {
-    return Objects.equals(model.getPathJson(), path) ? MEDIA_TYPE_JSON : MEDIA_TYPE_YAML;
-  }
+    private Response toResponse(String path, OpenApiModel model) {
+        var mediaType = getMediaType(path, model);
+        return Response.status(Response.Status.OK)
+                .entity(model.render(path))
+                .type(mediaType)
+                .build();
+    }
+
+    private Response notFound(String path) {
+        return Response
+                .status(Response.Status.NOT_FOUND)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .entity("No model at " + path).build();
+    }
+
+    private String getMediaType(String path, OpenApiModel model) {
+        return Objects.equals(model.getPathJson(), path) ? MEDIA_TYPE_JSON : MEDIA_TYPE_YAML;
+    }
 }
